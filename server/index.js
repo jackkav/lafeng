9203f18ef260fc31456faef1595705fdb44ebfc0
Jack Kavanagh <jackkav@gmail.com>
2017-03-20 22:47:46 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Constants
+const PORT = 3080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
