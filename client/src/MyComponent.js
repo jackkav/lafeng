fbd0b5961d2c27f3ecdd64e0739b9e949a462e91
Jack Kavanagh <jackkav@gmail.com>
2017-03-21 19:16:33 +0800

    added first component

---
+++ client/src/MyComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyComponent extends Component {
+  render() {
+    return (
+      <div className="App">
+        123
+      </div>
+    )
+  }
+}
