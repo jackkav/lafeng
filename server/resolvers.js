3492c2186508f3b6f402589ac30528b1dd6ebc38
Jack Kavanagh <jackkav@gmail.com>
2017-04-05 19:05:37 +0800

    switched to axios and added get series

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,23 @@
 import freshId from 'fresh-id'
-
+import axios from 'axios'
 export const resolverMap = {
   Query: {
+    async login(){
+      const a = await axios.post('https://api.thetvdb.com/login',
+      {
+          apikey: process.env.apikey,
+          user: process.env.user,
+          userKey: process.env.userKey 
+      })
+      if(a.status<400)
+      return a.data.token
+    },
+    async series(_, args){
+      const a = await axios.get(`https://api.thetvdb.com/series/${args.id}`,
+      {headers: {'Authorization': `Bearer ${args.token}`}})
+      if(a.status<400)
+      return a.data.data
+    },
     async visitor(_, args, { db }) {
       return db
         .collection('vistors')
